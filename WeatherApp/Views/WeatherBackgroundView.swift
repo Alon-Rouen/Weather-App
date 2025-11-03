d63bb294a7197c6d365b1149fd63da07d48aa54f
Alon-Rouen <alon.bennathan@groupe-esigelec.org>
2025-11-03 13:29:37 +0100

    Couleur + fond des icones

---
+++ WeatherApp/Views/WeatherBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherBackgroundView.swift
+//  WeatherApp
+//
+//  Created by codecamp on 03.11.25.
+//
+
+import SwiftUI
+
+struct WeatherBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherBackgroundView()
+}
